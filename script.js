d53be12e3758e785de55888c6d637be8e8ba7f14
marzec <gregmarzec02@gmail.com>
2022-06-24 23:00:06 -0400

    FIXED CHAR TYPE BUGS AND FIXED PASSLENGTH STATMENT

--- script.js
+++ script.js
@@ -13,38 +13,39 @@ function generatePassword() {
   var passLength = prompt("How many characters");
   if (passLength <= 7) {
     alert("Password must be atleast 8 characters");
+    return (generatePassword);
   } else if (passLength >= 129) {
     alert("please choose a password that has less than 128 characters");
+    return (generatePassword);
   }
 
   var passLower = confirm("do you want lowercase in the password?")
-    if (passLower = true) {
+    if (passLower) {
       alert("you will have lowercase");
       } else {
         alert("you will not have lowercase");
       }
     
       var passUpper = confirm("do you want uppercase in the password");
-      if (passUpper = true) {
+      if (passUpper) {
         alert("you will have uppercase");
       } else {
         alert("you will not have uppercase");
       }
     
     var passSpecial = confirm("do you want special characters in the password");
-    if (passSpecial = true) {
+    if (passSpecial) {
       alert("You will have special characters");
     } else {
       alert("you will not have special characters");
     }
     
     var passNum = confirm("do you want numbers in your password");
-    if (passNum = true) {
+    if (passNum) {
       alert("you will have numbers");
     } else {
       alert("you will not have numbers");
     }
-    return
 }
 
 // Write password to the #password input
