72ad421fc953a5ce2a6b714afbef4efcdc16c120
marzec <gregmarzec02@gmail.com>
2022-06-24 23:18:18 -0400

    changed variables into arrays, added comments

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-var charUP = "abcdefghijklmnopqrstuvwxyz";
-var charLOW = "ABCDEFGHIJKLMNOPQRSTUV";
-var specChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var numChar = "1234567890";
+//global variables
+var charLOW = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var charUP = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specChar = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","}","|","~"];
+var numChar = ["1","2","3","4","5","6","7","8","9","0"];
 
 
 
 function generatePassword() {
-  
+  //check user pass length
   var passLength = prompt("How many characters");
   if (passLength <= 7) {
     alert("Password must be atleast 8 characters");
@@ -18,33 +18,38 @@ function generatePassword() {
     alert("please choose a password that has less than 128 characters");
     return (generatePassword);
   }
-
+  //see if user wants lowercase
   var passLower = confirm("do you want lowercase in the password?")
     if (passLower) {
       alert("you will have lowercase");
       } else {
         alert("you will not have lowercase");
       }
-    
+    //see if user wants uppercase
       var passUpper = confirm("do you want uppercase in the password");
       if (passUpper) {
         alert("you will have uppercase");
       } else {
         alert("you will not have uppercase");
       }
-    
+    //see if user wants special characters
     var passSpecial = confirm("do you want special characters in the password");
     if (passSpecial) {
       alert("You will have special characters");
     } else {
       alert("you will not have special characters");
     }
-    
+    //see if user want numbers in password
     var passNum = confirm("do you want numbers in your password");
     if (passNum) {
       alert("you will have numbers");
     } else {
       alert("you will not have numbers");
+    }
+    //break out if no char type
+    if (passLower === false && passLower === false && passUpper === false && passSpecial === false) {
+      alert("you need atleast one char type");
+      return(generatePassword);
     }
 }
 
